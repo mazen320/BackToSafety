d3d08d5e2f2eba466ee5271c573204abc8fff4a3
mazen320 <mazenshaar@hotmail.com>
2022-06-05 13:46:54 +0400

    v0.6
    - added player punch
    - added ammunition system, bullet limit, reloading
    - added rifle pick up, made it start with punching when you start
    - added all the animation for the weopon side, shooting, walk shoot, etc.

--- Assets/_Scripts/Rifle.cs
+++ Assets/_Scripts/Rifle.cs
@@ -8,23 +8,82 @@ public class Rifle : MonoBehaviour
     public Camera cam;
     public float damage = 10f;
     public float bulletRange = 100f;
+    public float fireCharge = 15f;
+    private float nextTimeToShoot = 0;
+    public Animator anim;
+    public PlayerScript player;
+    public Transform hand;
+
+    [Header("Rifle shooting and ammunitition")]
+    private int maxAmmo = 32;
+    public int mag = 10;
+    private int currentAmmo;
+    public float reloadTime = 1.3f;
+    private bool isReloading = false;
 
     [Header("Rifle Effects")]
     public ParticleSystem muzzleFlash;
     public GameObject ConcreteEffect;
 
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        transform.SetParent(hand);
+        currentAmmo = maxAmmo;
+    }
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Shoot();
+        if (isReloading)
+            return;
 
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >=nextTimeToShoot)
+        {
+            anim.SetBool("Fire", true);
+            anim.SetBool("Idle", false);
+            nextTimeToShoot = Time.time + 1f / fireCharge;
+            Shoot();
+        }
+        else if(Input.GetButton("Fire1") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+            anim.SetBool("FireWalk", true);
+            anim.SetBool("Idle", false);
+        }
+        else if(Input.GetButton("Fire2") && Input.GetButton("Fire1"))
+            {
+            anim.SetBool("Idle", false);
+            anim.SetBool("IdleAim", true);
+            anim.SetBool("FireWalk", false);
+        }
+        else
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("FireWalk", false);
+            anim.SetBool("Fire", false);
         }
     }
     private void Shoot()
     {
+        //check the mag
+        if(mag == 0)
+        {
+            //show ammo in text
+            return;
+        }
+        currentAmmo--;
+
+        if(currentAmmo == 0)
+        {
+            mag--;
+        }
+        //update ui when implemented from here
+
         muzzleFlash.Play();
         RaycastHit hitInfo;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, bulletRange))
@@ -39,5 +98,23 @@ public class Rifle : MonoBehaviour
                 Destroy(impactShot, 1f);
             }
         }
+    }
+    IEnumerator Reload()
+    {
+        player.playerSpeed = 0f;
+        player.playerSprint = 0f;
+        isReloading = true;
+        Debug.Log("Reloading");
+
+        anim.SetBool("Reloading", true);
+        //anim
+        //sound
+        yield return new WaitForSeconds(reloadTime);
+        anim.SetBool("Reloading", false);
+        //anim
+        currentAmmo = maxAmmo;
+        player.playerSpeed = 1.9f;
+        player.playerSprint = 3;
+        isReloading = false;
     }
 }
--- Assets/_Scripts/SwitchCamera.cs
+++ Assets/_Scripts/SwitchCamera.cs
@@ -8,11 +8,30 @@ public class SwitchCamera : MonoBehaviour
     public GameObject aimCanvas;
     public GameObject thirdPersonCanvas;
     public GameObject thirdPersonCam;
+
+    public Animator anim;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire2"))
+        if (Input.GetButton("Fire2") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
+            anim.SetBool("Idle", false);
+            anim.SetBool("IdleAim", true);
+            anim.SetBool("RifleWalk", true);
+            anim.SetBool("Walk", true);
+
+            thirdPersonCam.SetActive(false);
+            thirdPersonCanvas.SetActive(false);
+            aimCam.SetActive(true);
+            aimCanvas.SetActive(true);
+        }
+        else if(Input.GetButton("Fire2"))
+        {
+            anim.SetBool("Idle", false);
+            anim.SetBool("IdleAim", true);
+            anim.SetBool("RifleWalk", false);
+            anim.SetBool("Walk", false);
+
             thirdPersonCam.SetActive(false);
             thirdPersonCanvas.SetActive(false);
             aimCam.SetActive(true);
@@ -20,6 +39,10 @@ public class SwitchCamera : MonoBehaviour
         }
         else
         {
+            anim.SetBool("Idle", true);
+            anim.SetBool("IdleAim", false);
+            anim.SetBool("RifleWalk", false);
+
             thirdPersonCam.SetActive(true);
             thirdPersonCanvas.SetActive(true);
             aimCam.SetActive(false);
